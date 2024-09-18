76cf7b680b3c5bc6c82ec5616599bdaa43c6e87b
harshnishad <harsh.nishad_cs21@gla.ac.in>
2024-09-18 22:07:28 +0530

    initial commit

---
+++ src/main/java/org/harshnishad/harshnishad/HarshNishadApplication.java
@@ -0,0 +1,13 @@
+package org.harshnishad.harshnishad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarshNishadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarshNishadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/harshnishad/harshnishad/HarshNishadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.harshnishad.harshnishad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarshNishadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
